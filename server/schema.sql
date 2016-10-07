62485e3084c5c8e1cf5157f9474ab9448364d68d
f-fong <feliciasfong@gmail.com>
2016-10-07 16:03:59 -0700

    Created database and message, users, and rooms tables

--- server/schema.sql
+++ server/schema.sql
@@ -4,7 +4,37 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int(100),
+  text VARCHAR(150),
+  user_id int(100),
+  room_id int(100),
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id int(100),
+  name VARCHAR(20),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id int(100),
+  name VARCHAR(20),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users_rooms (
+  /* Describe your table here.*/
+  user_id int(100),
+  room_id int(100)
+);
+
+ALTER TABLE messages ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE messages ADD FOREIGN KEY (room_id) REFERENCES rooms(id);
+ALTER TABLE users_rooms ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE users_rooms ADD FOREIGN KEY (room_id) REFERENCES rooms(id);
 
 /* Create other tables and define schemas for them here! */
 
