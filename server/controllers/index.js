1ed55b1bc66a4a89b412df5286577cc5699252b1
f-fong <feliciasfong@gmail.com>
2016-10-07 18:43:12 -0700

    Finish controller index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,16 +3,29 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {
+      models.messages.get(function(results) {
+        res.send(results);
+      });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-
+      models.messages.post(function(results) {
+        res.send(results);
+      });
     } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      models.users.get(function(results) {
+        res.send(results);
+      });
+    },
+    post: function (req, res) {
+      models.users.post(function(results) {
+        res.send(results);
+      });
+    }
   }
 };
 
